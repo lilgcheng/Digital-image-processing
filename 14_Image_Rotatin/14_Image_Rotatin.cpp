d0f2c1b4b0e24f7ba5b0a421cf5d74ecd4aefd97
liancheng <lilgcheng@gmail.com>
2017-04-22 21:44:41 +0800

    add rotaion

--- 14_Image_Rotatin/14_Image_Rotatin.cpp
+++ 14_Image_Rotatin/14_Image_Rotatin.cpp
@@ -12,10 +12,13 @@ int histogram_equalization[MaxBMPSizeX][MaxBMPSizeY]; // MaxBMPSizeX and MaxBMPS
 int R[MaxBMPSizeX][MaxBMPSizeY];
 int G[MaxBMPSizeX][MaxBMPSizeY];
 int B[MaxBMPSizeX][MaxBMPSizeY];
+int R1[MaxBMPSizeX][MaxBMPSizeY];
+int G1[MaxBMPSizeX][MaxBMPSizeY];
+int B1[MaxBMPSizeX][MaxBMPSizeY];
 int r[1024][1024];
 int g[1024][1024];
 int b[1024][1024];
-int gray[1024][1024];
+
 int arr[1024][1024];
 void img_to_gray(int w, int h);
 int main()
@@ -37,63 +40,73 @@ int main()
 	double angle = degree*PI/180;
 	
 	open_bmp("framed_airplane.bmp", R, G, B, width, height); // for gray images
-	img_to_gray(width,height); 
-	save_bmp("framed_airplane_gray.bmp", gray, gray, gray);
-	int x, y, nx, ny;
-// 	for(ny=0; ny<height; ++ny){
-//        for(nx=0; nx<width; ++nx){
-//            x = (int)(nx * cos(angle) - ny * sin(angle) + 0.5);
-//            y = (int)(nx * sin(angle) + ny * cos(angle) + 0.5);
-//            if(y>=0 && y<height && x>=0 && x<width){
-//				arr[ny][nx]=gray[y][x];
-//            } else {
-//				arr[ny][nx]=255;
-//            }
-//        }
-//    }
-//	save_bmp("framed_airplane_gray_原點逆時針.bmp", arr, arr, arr);
-    
     int ox = (width-1)/2; //新原點ox 
     int oy = (height-1)/2;//新原點oy 
  	int nx2,ny2;
- 	int p1,p2;//計算內插使用
-	  
-//	for(ny=0; ny<height; ++ny){
-//        for(nx=0; nx<width; ++nx){
-//    		nx2 = nx - ox;
-//			ny2 = ny - oy;
-//	        x = (int)( nx2  * cos(angle) + ny2 * sin(angle) +ox);
-//            y = (int)(-nx2  * sin(angle) + ny2 * cos(angle) +oy);
-//            if(y>=0 && y<height && x>=0 && x<width){
-//				arr[ny][nx]=gray[x][y];
-//            }
-//			else {
-//				arr[ny][nx]=255;
-//            }
-//        }
-//    }
-//    save_bmp("framed_airplane_gray_平移原點逆時針.bmp", arr, arr, arr);
-//    
-    for(ny=0; ny<height; ++ny){
+ 	int p1,p2,p3,p4,p5,p6;//計算內插使用
+	int x, y, nx, ny;
+ 	for(ny=0; ny<height; ++ny){
         for(nx=0; nx<width; ++nx){
-    		nx2 = nx - ox;
-			ny2 = ny - oy;
-	        x = (int)( nx2  * cos(angle) + ny2 * sin(angle) + 0.5+ox);
-            y = (int)(-nx2  * sin(angle) + ny2 * cos(angle) + 0.5+oy);
+    		nx2 = nx - ox;//平移X圖片中心 
+			ny2 = ny - oy;//平移y圖片中心
+	        x = (int)( nx2  * cos(angle) + ny2 * sin(angle) +ox);//
+            y = (int)(-nx2  * sin(angle) + ny2 * cos(angle) +oy);//
             if(y>=0 && y<height && x>=0 && x<width){
-				p1 = gray[int(x)][int(y)] * abs(y - int(y+1)) + 
-					gray[int(x)][int(y+1)] * abs(y - int(y));
-
-				p2 = gray[int(x+1)][int(y)] * abs(y - int(y+1)) +
-					gray[int(x+1)][int(y+1)] * abs(y - int(y));
-//				arr[x][y] = p1 * abs(x - int(nx+1)) + p2 * abs(x - int(x));
-				arr[x][y]=gray[ny][nx];
+				R1[x][y]=R[ny][nx];
+				G1[x][y]=G[ny][nx];
+				B1[x][y]=B[ny][nx];
             } else {
-				arr[x][y]=255;
+				R1[x][y]=255;
+				G1[x][y]=255;
+				B1[x][y]=255;
             }
         }
     }
-    save_bmp("framed_airplane_gray_內插.bmp", arr, arr, arr);
+	save_bmp("framed_airplane_space_原點逆時針.bmp", R1, G1, B1);
+    
+	
+    for(ny=0; ny<height; ++ny){
+        for(nx=0; nx<width; ++nx){
+    		nx2 = nx - ox;//平移X圖片中心 
+			ny2 = ny - oy;//平移y圖片中心
+	        x = (int)( nx2  * cos(angle) - ny2 * sin(angle) +ox);//為原圖對應座標 
+            y = (int)(nx2  * sin(angle) + ny2 * cos(angle) +oy);//為
+            if(y>=0 && y<height && x>=0 && x<width){
+            
+				p1 = R[int(x)][int(y)] * abs(y - int(y+1)) + 
+ 					R[int(x)][int(y+1)] * abs(y - int(y));
+ 
+ 				p2 = R[int(x+1)][int(y)] * abs(y - int(y+1)) +
+ 					R[int(x+1)][int(y+1)] * abs(y - int(y));
+ 				
+ 				p3 = G[int(x)][int(y)] * abs(y - int(y+1)) + 
+ 					G[int(x)][int(y+1)] * abs(y - int(y));
+ 
+ 				p4 = G[int(x+1)][int(y)] * abs(y - int(y+1)) +
+ 					G[int(x+1)][int(y+1)] * abs(y - int(y));
+ 					
+ 				p5 = B[int(x)][int(y)] * abs(y - int(y+1)) + 
+ 					B[int(x)][int(y+1)] * abs(y - int(y));
+ 
+ 				p6 = B[int(x+1)][int(y)] * abs(y - int(y+1)) +
+ 					B[int(x+1)][int(y+1)] * abs(y - int(y));
+
+				R1[nx][ny]=p1 * abs(x - int(x+1)) + p2 * abs(x - int(x));
+				G1[nx][ny]=p3 * abs(x - int(x+1)) + p4 * abs(x - int(x));
+				B1[nx][ny]=p5 * abs(x - int(x+1)) + p6 * abs(x - int(x));
+//			
+//				R1[nx][ny]=R[x][y];
+//				G1[nx][ny]=G[x][y];
+//				B1[nx][ny]=B[x][y];
+            } else {
+				R1[x][y]=255;
+				G1[x][y]=255;
+				B1[x][y]=255;
+            }
+        }
+    }
+    save_bmp("framed_airplane_gray_內插no.bmp", R1, G1, B1);
+    save_bmp("arr.bmp", arr, arr, arr);
 	close_bmp();
 	END = clock();
 	printf("進行運算所花費的時間：%f S\n", (END - START) / CLOCKS_PER_SEC);
@@ -102,11 +115,3 @@ int main()
 }
 
 
-void img_to_gray(int w, int h){
-	int i, j;
-	for (i = 0; i < w; i++){
-		for (j = 0; j < h; j++){
-			gray[i][j] = ((int)((0.299*R[i][j] + 0.587*G[i][j] + 0.114*B[i][j]) * 1000) / 1000);
-		}
-	}
-}
